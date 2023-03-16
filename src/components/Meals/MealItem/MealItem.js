fe2aa07586c4373756e023f47f8d0f9d22753759
Sergii <sergii.khylko@outlook.com>
2023-03-16 20:21:47 +0200

    #5 add form for meal items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./MealItem.module.css"
+import MealItemForm from "./MealItemForm";
 
 const MealItem = props => {
   const formattedPrice = `$${props.price.toFixed(2)}`;
@@ -14,7 +15,7 @@ const MealItem = props => {
         <div className={styles.price}>{formattedPrice}</div>
       </div>
       <div>
-
+        <MealItemForm id={props.id}/>
       </div>
     </li>
   )
