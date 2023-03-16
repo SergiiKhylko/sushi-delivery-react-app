5ca168caecaa6cd6fa524022ea8599bdcf2fd601
Sergii <sergii.khylko@outlook.com>
2023-03-16 23:58:36 +0200

    #6 add modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <React.Fragment>
-      <Header/>
+      <Cart />
+      <Header />
       <main>
         <Meals />
       </main>
