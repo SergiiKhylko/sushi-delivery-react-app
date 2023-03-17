d99dcebc7da17c70921ac270ca6e5dedad6ebd79
Sergii <sergii.khylko@outlook.com>
2023-03-17 17:58:20 +0200

    #11 add counting same items in the cart

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -8,8 +8,32 @@ const defaultCardState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD_ITEM") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCardItemIndex = state.items.findIndex(item => {
+      return item.id === action.item.id;
+    });
+
+    const existingCardItem = state.items[existingCardItemIndex];
+
+    let updatedItem;
+    let updatedItems;
+
+    if (existingCardItem) {
+      updatedItem = {
+        ...existingCardItem,
+        amount: existingCardItem.amount + action.item.amount
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCardItemIndex] = updatedItem
+    } else {
+      updatedItem = {
+        ...action.item
+      }
+      updatedItems = state.items.concat(updatedItem);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
